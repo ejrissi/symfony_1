1d9c0802d63b27312e4323a628fb244e198b5a5a
ahmed <ahmedmejrissi2023@gmail.com>
2025-03-12 23:45:21 +0100

    Describe what you updated

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -8,11 +8,12 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
-    #[Route('/{name}', name: 'homepage')]
-    public function home(string $name): Response
+    #[Route('/', name: 'home')]
+    public function home(): Response
     {
-        return $this->render('index.html.twig', [
-            'name' => $name,
+        $articles = ['Article 1', 'Article 2', 'Article 3'];
+        return $this->render('articles/index.html.twig', [
+            'articles' => $articles,
         ]);
     }
 }
